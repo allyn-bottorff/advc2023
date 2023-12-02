02f7c722995f026de5445f313165594e660136dc
Allyn L. Bottorff <allyn@b6f.net>
2023-12-02 16:29:27 -0600

    day2 pt2 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -2,11 +2,40 @@ use std::fs::File;
 use std::io::{self, BufRead};
 use std::path::Path;
 
-#[derive(Debug)]
+#[derive(Debug, PartialEq)]
 struct Bag {
     red: u32,
     green: u32,
     blue: u32,
+}
+impl Bag {
+    fn from_game(game: &Game) -> Self {
+
+        let mut red_max = 0;
+        let mut blue_max = 0;
+        let mut green_max = 0;
+
+        for pick in &game.picks {
+            if pick.red > red_max {
+                red_max = pick.red
+            }
+            if pick.blue > blue_max {
+                blue_max = pick.blue
+            }
+            if pick.green > green_max {
+                green_max = pick.green
+            }
+        }
+
+        Bag {
+            red: red_max,
+            green: green_max,
+            blue: blue_max,
+        }
+    }
+    fn power(self) -> u32 {
+        self.red * self.blue * self.green
+    }
 }
 
 #[derive(Debug)]
@@ -15,7 +44,7 @@ struct Game {
     picks: Vec<Pick>,
 }
 impl Game {
-    fn new(line: String) -> Self {
+    fn new(line: &String) -> Self {
         let mut game = Game {
             number: 0,
             picks: Vec::new(),
@@ -63,7 +92,7 @@ fn main() {
     
 
     let mut sum = 0;
-    for line in lines {
+    for line in &lines {
         let game = Game::new(line);
         if pt1_validate_game(&game, &bag) {
             sum += &game.number;
@@ -71,6 +100,15 @@ fn main() {
     }
 
     println!("Part 1 sum: {}", sum);
+
+    let mut sum = 0;
+    for line in &lines {
+        let game = Game::new(line);
+        let bag = Bag::from_game(&game);
+        sum += bag.power();
+    }
+
+    println!("Part 2 sum: {}", sum);
 
 }
 
@@ -116,7 +154,7 @@ mod tests {
     fn test_pt1_game_1_validate() {
         let game_text = String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
 
-        let game = Game::new(game_text);
+        let game = Game::new(&game_text);
         let bag = Bag {
             red: 12,
             green: 13,
@@ -130,7 +168,7 @@ mod tests {
         let game_text =
             String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue");
 
-        let game = Game::new(game_text);
+        let game = Game::new(&game_text);
         let bag = Bag {
             red: 12,
             green: 13,
@@ -140,12 +178,12 @@ mod tests {
         assert_eq!(pt1_validate_game(&game, &bag), true)
     }
     #[test]
-    fn test_pt2_game_3_validate() {
+    fn test_pt1_game_3_validate() {
         let game_text = String::from(
             "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
         );
 
-        let game = Game::new(game_text);
+        let game = Game::new(&game_text);
         let bag = Bag {
             red: 12,
             green: 13,
@@ -155,12 +193,12 @@ mod tests {
         assert_eq!(pt1_validate_game(&game, &bag), false)
     }
     #[test]
-    fn test_pt2_game_4_validate() {
+    fn test_pt1_game_4_validate() {
         let game_text = String::from(
             "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
         );
 
-        let game = Game::new(game_text);
+        let game = Game::new(&game_text);
         let bag = Bag {
             red: 12,
             green: 13,
@@ -173,7 +211,7 @@ mod tests {
     fn test_pt1_game_5_validate() {
         let game_text = String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
 
-        let game = Game::new(game_text);
+        let game = Game::new(&game_text);
         let bag = Bag {
             red: 12,
             green: 13,
@@ -181,5 +219,22 @@ mod tests {
         };
 
         assert_eq!(pt1_validate_game(&game, &bag), true)
+    }
+
+    #[test]
+    fn test_p2_bag_1() {
+        let game_text = String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+        let game = Game::new(&game_text);
+
+        let bag = Bag::from_game(&game);
+
+        let bag_literal = Bag {
+            red: 4,
+            green: 2,
+            blue: 6,
+
+        };
+        assert_eq!(bag, bag_literal)
+
     }
 }
