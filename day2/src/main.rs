c759d8361f59133be2282620b9a783fcc904de4b
Allyn L. Bottorff <allyn@b6f.net>
2023-12-02 14:10:14 -0600

    part 1 solution

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -1,7 +1,6 @@
-use std::path::Path;
 use std::fs::File;
-use std::io::{self,BufRead};
-
+use std::io::{self, BufRead};
+use std::path::Path;
 
 #[derive(Debug)]
 struct Bag {
@@ -16,7 +15,7 @@ struct Game {
     picks: Vec<Pick>,
 }
 impl Game {
-    fn new(line: String) -> Self{
+    fn new(line: String) -> Self {
         let mut game = Game {
             number: 0,
             picks: Vec::new(),
@@ -26,13 +25,13 @@ impl Game {
         game.number = number.parse::<u32>().unwrap();
 
         for pick_str in picks.split(";") {
-            let mut pick = Pick{
+            let mut pick = Pick {
                 red: 0,
                 green: 0,
                 blue: 0,
             };
             for color in pick_str.split(",") {
-                let color_parts: Vec<&str>= color.split(" ").collect();
+                let color_parts: Vec<&str> = color.split(" ").collect();
                 match color_parts[2] {
                     "blue" => pick.blue = color_parts[1].parse::<u32>().unwrap(),
                     "red" => pick.red = color_parts[1].parse::<u32>().unwrap(),
@@ -54,17 +53,42 @@ struct Pick {
 }
 
 fn main() {
+    let lines = read_lines("input.txt");
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
 
-    // let lines = read_lines("input.txt");
-    let game1_text = String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    
 
-    let game = Game::new(game1_text);
+    let mut sum = 0;
+    for line in lines {
+        let game = Game::new(line);
+        if pt1_validate_game(&game, &bag) {
+            sum += &game.number;
+        }
+    }
 
-
+    println!("Part 1 sum: {}", sum);
 
 }
 
-
+// Make sure all games are valid for the given bag contents
+fn pt1_validate_game(game: &Game, bag: &Bag) -> bool {
+    for p in &game.picks {
+        if p.red > bag.red {
+            return false
+        }
+        if p.blue > bag.blue {
+            return false
+        }
+        if p.green > bag.green {
+            return false 
+        }
+    }
+    true
+}
 
 //Read all lines into a vec of strings. Crash if something isn't right.
 fn read_lines<P>(filename: P) -> Vec<String>
@@ -77,32 +101,85 @@ where
     buf.lines().map(|l| l.unwrap()).collect()
 }
 
-
-
-
-
-
-
 #[cfg(test)]
 mod tests {
     use super::*;
+    /*
+    Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+    Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+    Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+    Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+    Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
+    */
+
     #[test]
-/*
-Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
-Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
-Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
-Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
-Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green
-*/
-    fn test_pt1_parse_game() {
-        let game1_text = String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
+    fn test_pt1_game_1_validate() {
+        let game_text = String::from("Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green");
 
-        let game = Game::new(game1_text);
+        let game = Game::new(game_text);
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
 
-
-
-
-
+        assert_eq!(pt1_validate_game(&game, &bag), true)
     }
+    #[test]
+    fn test_pt1_game_2_validate() {
+        let game_text =
+            String::from("Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue");
 
+        let game = Game::new(game_text);
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        assert_eq!(pt1_validate_game(&game, &bag), true)
+    }
+    #[test]
+    fn test_pt2_game_3_validate() {
+        let game_text = String::from(
+            "Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red",
+        );
+
+        let game = Game::new(game_text);
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        assert_eq!(pt1_validate_game(&game, &bag), false)
+    }
+    #[test]
+    fn test_pt2_game_4_validate() {
+        let game_text = String::from(
+            "Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red",
+        );
+
+        let game = Game::new(game_text);
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        assert_eq!(pt1_validate_game(&game, &bag), false)
+    }
+    #[test]
+    fn test_pt1_game_5_validate() {
+        let game_text = String::from("Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green");
+
+        let game = Game::new(game_text);
+        let bag = Bag {
+            red: 12,
+            green: 13,
+            blue: 14,
+        };
+
+        assert_eq!(pt1_validate_game(&game, &bag), true)
+    }
 }
