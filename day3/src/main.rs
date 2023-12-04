3da0da7c9f1e11f059d1aec28e8e87a7126d9776
Allyn L. Bottorff <allyn@b6f.net>
2023-12-03 22:49:04 -0600

    day 2 pt2 solution

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -20,33 +20,49 @@ fn main() {
         .collect();
 
     let mut sum = 0;
+    let mut power_sum = 0;
 
     for (i, line) in lines.into_iter().enumerate() {
-        sum += find_adjacent_parts(&line, i, &mut parts);
+        let (part_val, power) = find_adjacent_parts(&line, i, &mut parts);
+        sum += part_val;
+        power_sum += power;
     }
 
-    println!("Pt 1. sum: {}", sum);
+    println!("Pt 1 sum: {}", sum);
+    println!("Pt. 2 sum: {}", power_sum);
 }
 
 /// Traverse a line and find symbols. Then check if the adjacent cells are
 /// contained within a part.
-fn find_adjacent_parts(line: &String, l_num: usize, parts: &mut Vec<Vec<PartNumber>>) -> u32 {
-    let mut sum = 0;
+fn find_adjacent_parts(
+    line: &String,
+    l_num: usize,
+    parts: &mut Vec<Vec<PartNumber>>,
+) -> (u32, u32) {
+    let mut part_sum = 0;
+    let mut power_sum = 0;
     for (i, c) in line.chars().enumerate() {
         if !c.is_ascii_digit() && c != '.' {
+            let mut adj = Vec::new();
             // Check current line
             for part in &mut parts[l_num] {
                 if i > 0 {
                     if part.start <= i - 1 && part.end >= i - 1 {
                         if !part.found {
-                            sum += part.num;
+                            if c == '*' {
+                                adj.push(part.num.clone());
+                            }
+                            part_sum += part.num;
                             part.found = true;
                         }
                     }
                 }
                 if part.start <= i + 1 && part.end >= i + 1 {
                     if !part.found {
-                        sum += part.num;
+                        if c == '*' {
+                            adj.push(part.num.clone());
+                        }
+                        part_sum += part.num;
                         part.found = true;
                     }
                 }
@@ -57,20 +73,29 @@ fn find_adjacent_parts(line: &String, l_num: usize, parts: &mut Vec<Vec<PartNumb
                     if i > 0 {
                         if part.start <= i - 1 && part.end >= i - 1 {
                             if !part.found {
-                                sum += part.num;
+                                if c == '*' {
+                                    adj.push(part.num.clone());
+                                }
+                                part_sum += part.num;
                                 part.found = true;
                             }
                         }
                     }
                     if part.start <= i && part.end >= i {
                         if !part.found {
-                            sum += part.num;
+                            if c == '*' {
+                                adj.push(part.num.clone());
+                            }
+                            part_sum += part.num;
                             part.found = true;
                         }
                     }
                     if part.start <= i + 1 && part.end >= i + 1 {
                         if !part.found {
-                            sum += part.num;
+                            if c == '*' {
+                                adj.push(part.num.clone());
+                            }
+                            part_sum += part.num;
                             part.found = true;
                         }
                     }
@@ -82,29 +107,42 @@ fn find_adjacent_parts(line: &String, l_num: usize, parts: &mut Vec<Vec<PartNumb
                     if i > 0 {
                         if part.start <= i - 1 && part.end >= i - 1 {
                             if !part.found {
-                                sum += part.num;
+                                if c == '*' {
+                                    adj.push(part.num.clone());
+                                }
+                                part_sum += part.num;
                                 part.found = true;
                             }
                         }
                     }
                     if part.start <= i && part.end >= i {
                         if !part.found {
-                            sum += part.num;
+                            if c == '*' {
+                                adj.push(part.num.clone());
+                            }
+                            part_sum += part.num;
                             part.found = true;
                         }
                     }
                     if part.start <= i + 1 && part.end >= i + 1 {
                         if !part.found {
-                            sum += part.num;
+                            if c == '*' {
+                                adj.push(part.num.clone());
+                            }
+                            part_sum += part.num;
                             part.found = true;
                         }
                     }
                 }
             }
+            if adj.len() == 2 {
+                let power = adj[0] * adj[1];
+                power_sum += power;
+            }
         }
     }
 
-    sum
+    (part_sum, power_sum)
 }
 
 /// Read all lines into a Vec<String> crash if something isn't right
@@ -186,7 +224,8 @@ mod tests {
             .collect();
 
         for (i, line) in test_lines.into_iter().enumerate() {
-            sum += find_adjacent_parts(&line, i, &mut parts);
+            let (part_sum, _) = find_adjacent_parts(&line, i, &mut parts);
+            sum += part_sum;
         }
 
         let expected = 4361;
@@ -212,7 +251,7 @@ mod tests {
             .into_iter()
             .map(|l| find_numbers(&l))
             .collect();
-        let sum = find_adjacent_parts(&test_lines[0], 0, &mut parts);
+        let (sum, _) = find_adjacent_parts(&test_lines[0], 0, &mut parts);
         assert_eq!(0, sum)
     }
 
@@ -235,7 +274,7 @@ mod tests {
             .into_iter()
             .map(|l| find_numbers(&l))
             .collect();
-        let sum = find_adjacent_parts(&test_lines[1], 1, &mut parts);
+        let (sum, _) = find_adjacent_parts(&test_lines[1], 1, &mut parts);
         assert_eq!(467 + 35 + 633, sum)
     }
     #[test]
@@ -257,7 +296,7 @@ mod tests {
             .into_iter()
             .map(|l| find_numbers(&l))
             .collect();
-        let sum = find_adjacent_parts(&test_lines[3], 3, &mut parts);
+        let (sum, _) = find_adjacent_parts(&test_lines[3], 3, &mut parts);
         assert_eq!(633, sum)
     }
 
@@ -280,8 +319,76 @@ mod tests {
             .into_iter()
             .map(|l| find_numbers(&l))
             .collect();
-        let sum = find_adjacent_parts(&test_lines[8], 8, &mut parts);
+        let (sum, _) = find_adjacent_parts(&test_lines[8], 8, &mut parts);
         assert_eq!(664 + 598 + 755, sum)
+    }
+
+    #[test]
+    fn pt2_find_gear_power_1() {
+        let test_lines = vec![
+            String::from("467..114.."),
+            String::from("...*......"),
+            String::from("..35..633."),
+            String::from("......#..."),
+            String::from("617*......"),
+            String::from(".....+.58."),
+            String::from("..592....."),
+            String::from("......755."),
+            String::from("...$.*...."),
+            String::from(".664.598.."),
+        ];
+        let mut parts: Vec<Vec<PartNumber>> = test_lines
+            .clone()
+            .into_iter()
+            .map(|l| find_numbers(&l))
+            .collect();
+        let (_, power) = find_adjacent_parts(&test_lines[1], 1, &mut parts);
+        assert_eq!(467 * 35, power)
+    }
+
+    #[test]
+    fn pt2_find_gear_power_2() {
+        let test_lines = vec![
+            String::from("467..114.."),
+            String::from("...*......"),
+            String::from("..35..633."),
+            String::from("......#..."),
+            String::from("617*......"),
+            String::from(".....+.58."),
+            String::from("..592....."),
+            String::from("......755."),
+            String::from("...$.*...."),
+            String::from(".664.598.."),
+        ];
+        let mut parts: Vec<Vec<PartNumber>> = test_lines
+            .clone()
+            .into_iter()
+            .map(|l| find_numbers(&l))
+            .collect();
+        let (_, power) = find_adjacent_parts(&test_lines[2], 2, &mut parts);
+        assert_eq!(0, power)
+    }
+    #[test]
+    fn pt2_find_gear_power_8() {
+        let test_lines = vec![
+            String::from("467..114.."),
+            String::from("...*......"),
+            String::from("..35..633."),
+            String::from("......#..."),
+            String::from("617*......"),
+            String::from(".....+.58."),
+            String::from("..592....."),
+            String::from("......755."),
+            String::from("...$.*...."),
+            String::from(".664.598.."),
+        ];
+        let mut parts: Vec<Vec<PartNumber>> = test_lines
+            .clone()
+            .into_iter()
+            .map(|l| find_numbers(&l))
+            .collect();
+        let (_, power) = find_adjacent_parts(&test_lines[8], 8, &mut parts);
+        assert_eq!(755 * 598, power)
     }
 
     #[test]
