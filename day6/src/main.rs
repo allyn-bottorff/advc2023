d0bc8d62ecdb2c35e1193378951063b4a695f26d
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-12-06 13:47:04 -0600

    day 6 pt 2 solution. Skipped parsing the input data for the second day because it was so small

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    let content = std::fs::read_to_string("input.txt").unwrap();
+    let content = std::fs::read_to_string("example.txt").unwrap();
     let times: Vec<i32> = content
         .lines()
         .next()
@@ -37,6 +37,14 @@ fn main() {
     let margin = margins.iter().copied().reduce(|acc, e| acc * e).unwrap();
 
     println!("margin: {}", margin);
+
+    let race: (i64, i64) = (61677571,430103613071150);
+
+    let count = count_winning_press_times(&race);
+
+    println!("count: {}", count);
+
+    
 }
 
 fn find_winning_press_times(race: &(i32, i32)) -> Vec<i32> {
@@ -61,6 +69,29 @@ fn find_winning_press_times(race: &(i32, i32)) -> Vec<i32> {
     winning_press_times
 }
 
+fn count_winning_press_times(race: &(i64, i64)) -> u64 {
+    /*
+     * d = v * t
+     * v = (press)
+     * t = race_time - press
+     * d = press * (race_time - press)
+     *
+     */
+
+    let (r_time, r_record) = race;
+    let mut count = 0;
+
+
+    for p in 1..=*r_time {
+        if p * (r_time - p) > *r_record {
+            count += 1;
+        }
+    }
+
+    count
+
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -70,5 +101,12 @@ mod tests {
         let winning_times = find_winning_press_times(&(7, 9));
 
         assert_eq!(vec![2, 3, 4, 5], winning_times)
+    }
+    #[test]
+    fn test_count_winning_press_times() {
+        let count = count_winning_press_times(&(71530,940200));
+        assert_eq!(71503, count)
+
+
     }
 }
