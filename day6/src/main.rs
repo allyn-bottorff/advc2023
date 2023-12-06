556c478760c4d418c4fe2fc81d60ea8a1ecf606b
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-12-06 13:51:00 -0600

    converted day 6 pt1 to use pt2's function instead

--- day6/src/main.rs
+++ day6/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
-    let content = std::fs::read_to_string("example.txt").unwrap();
-    let times: Vec<i32> = content
+    let content = std::fs::read_to_string("input.txt").unwrap();
+    let times: Vec<i64> = content
         .lines()
         .next()
         .unwrap()
@@ -8,10 +8,10 @@ fn main() {
         .unwrap()
         .1
         .split_whitespace()
-        .map(|x| x.parse::<i32>().unwrap())
+        .map(|x| x.parse::<i64>().unwrap())
         .collect();
 
-    let distances: Vec<i32> = content
+    let distances: Vec<i64> = content
         .lines()
         .last()
         .unwrap()
@@ -19,19 +19,19 @@ fn main() {
         .unwrap()
         .1
         .split_whitespace()
-        .map(|x| x.parse::<i32>().unwrap())
+        .map(|x| x.parse::<i64>().unwrap())
         .collect();
 
     //Race records Vec<(time, distance_record)>
-    let race_records: Vec<(i32, i32)> = std::iter::zip(times, distances).collect();
+    let race_records: Vec<(i64, i64)> = std::iter::zip(times, distances).collect();
 
     println!("race records: {:?}", race_records);
 
     let mut margins = Vec::new();
 
     for r in &race_records {
-        let w_press = find_winning_press_times(r);
-        margins.push(w_press.len());
+        let w_press_count = count_winning_press_times(r);
+        margins.push(w_press_count);
     }
 
     let margin = margins.iter().copied().reduce(|acc, e| acc * e).unwrap();
@@ -47,27 +47,6 @@ fn main() {
     
 }
 
-fn find_winning_press_times(race: &(i32, i32)) -> Vec<i32> {
-    /*
-     * d = v * t
-     * v = (press)
-     * t = race_time - press
-     * d = press * (race_time - press)
-     *
-     */
-
-    let (r_time, r_record) = race;
-
-    let mut winning_press_times = Vec::new();
-
-    for p in 1..=*r_time {
-        if p * (r_time - p) > *r_record {
-            winning_press_times.push(p);
-        }
-    }
-
-    winning_press_times
-}
 
 fn count_winning_press_times(race: &(i64, i64)) -> u64 {
     /*
@@ -96,12 +75,6 @@ fn count_winning_press_times(race: &(i64, i64)) -> u64 {
 mod tests {
     use super::*;
 
-    #[test]
-    fn test_find_winning_press_times() {
-        let winning_times = find_winning_press_times(&(7, 9));
-
-        assert_eq!(vec![2, 3, 4, 5], winning_times)
-    }
     #[test]
     fn test_count_winning_press_times() {
         let count = count_winning_press_times(&(71530,940200));
