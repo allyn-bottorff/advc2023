d70cad6ee9a32dab0b311a32d3904f0a0dce2e12
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-12-12 16:38:02 -0600

    in progress

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,26 @@
+#[derive(Debug)]
+struct Record {
+    rules: Vec<u8>,
+    springs: Vec<char>,
+}
+
+impl From<&str> for Record {
+    fn from(value: &str) -> Self {
+        let (spring_str, rules_str) = value.split_once(" ").unwrap();
+        let springs: Vec<char> = spring_str.chars().collect();
+        let rules: Vec<u8> = rules_str
+            .split(",")
+            .map(|x| x.parse::<u8>().unwrap())
+            .collect();
+
+        Record { rules, springs }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let contents = std::fs::read_to_string("example.txt").unwrap();
+    let records: Vec<Record> = contents.lines().map(|l| Record::from(l)).collect();
+
+    println!("{:?}", records);
+    
 }
