ebd933dd8f215bf02c69cac6e63f193bb8ebf1bf
Allyn L. Bottorff <allyn.bottorff@veteransunited.com>
2023-12-15 15:44:11 -0600

    in progress

--- day12/src/main.rs
+++ day12/src/main.rs
@@ -1,3 +1,6 @@
+use itertools::Itertools;
+use std::ops::Range;
+
 #[derive(Debug)]
 struct Record {
     rules: Vec<u8>,
@@ -11,7 +14,10 @@ impl From<&str> for Record {
             .split(",")
             .map(|x| x.parse::<u8>().unwrap())
             .collect();
-        Record { rules, springs: spring_str.to_string() }
+        Record {
+            rules,
+            springs: spring_str.to_string(),
+        }
     }
 }
 
@@ -20,7 +26,59 @@ fn main() {
     let records: Vec<Record> = contents.lines().map(|l| Record::from(l)).collect();
 
     println!("{:?}", records);
+
+    let test_string = String::from("????");
+    let mut chars = vec!['.'; test_string.len()];
+    let mut pounds = vec!['#'; test_string.len()];
+    chars.append(&mut pounds);
+    let result: Vec<String> = chars
+        .into_iter()
+        .permutations(test_string.len())
+        .map(|x| x.into_iter().collect::<String>())
+        .unique()
+        .collect();
+    println!();
+
+    for line in &records{
+        println!("{:?}", line.springs);
+    }
+
+    let mut qs: Vec<Vec<Range<usize>>> = Vec::new();
+    for record in &records {
+
+        let chars: Vec<char> = record.springs.chars().collect();
+        let mut prev_q = false;
+        let mut range: Range<usize> = 0..record.springs.len();
+        let mut ranges: Vec<Range<usize>> = Vec::new();
+        for i in 0..record.springs.len() {
+            if chars[i] == '?' {
+                if prev_q == false {
+                    range.start = i;
+                }
+                prev_q = true;
+            } else {
+                if prev_q == true {
+                    range.end = i;
+                }
+                prev_q = true;
+            }
+            ranges.push(range.clone());
+        }
+        qs.push(ranges.clone());
+    }
+
+    println!("q ranges:");
+    for q in qs {
+        println!("{:?}", q);
+    }
+
+
 }
+
+// fn get_options(springs: &String) -> Vec<String> {
+//     let unknowns: Vec<String>
+//
+// }
 
 fn test_record(springs: &String, rules: &Vec<u8>) -> bool {
     let springs: Vec<_> = springs.split('.').collect();
@@ -30,7 +88,6 @@ fn test_record(springs: &String, rules: &Vec<u8>) -> bool {
     &lengths == rules
 }
 
-
 #[cfg(test)]
 mod tests {
     use super::*;
@@ -38,7 +95,7 @@ mod tests {
     #[test]
     fn test_test_record_pass() {
         let springs = String::from("#.#.###");
-        let rules: Vec<u8> = vec![1,1,3];
+        let rules: Vec<u8> = vec![1, 1, 3];
 
         assert_eq!(test_record(&springs, &rules), true)
     }
@@ -46,7 +103,7 @@ mod tests {
     #[test]
     fn test_test_record_fail() {
         let springs = String::from("#.#.###");
-        let rules: Vec<u8> = vec![1,1,4];
+        let rules: Vec<u8> = vec![1, 1, 4];
 
         assert_eq!(test_record(&springs, &rules), false)
     }
